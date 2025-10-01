0212d60aaac406a77a05b1e8f8977c42de0e4bcd
Bashu-011 <bashu2609@gmail.com>
2025-10-01 08:31:28 +0300

    Milestone 3 - First commit

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
 
-# Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for managing listings.
+    Provides CRUD: list, create, retrieve, update, delete
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for managing bookings.
+    Provides CRUD: list, create, retrieve, update, delete
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
